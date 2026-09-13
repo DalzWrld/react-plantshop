4466c36d55c2a80fbbe4502e5888df9878514757
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-09-13 23:24:50 +0300

    Update the imports in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import Header from "./components/Header";
-import PlantPage from "./components/PlantPage";
+import Header from "./Header";
+import PlantPage from "./PlantPage";
 
 function App() {
   const [plants, setPlants] = useState([]);
