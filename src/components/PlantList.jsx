fdbcbaef69292a699c5b5fa8251f0ab6c86fbd22
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-09-13 23:16:17 +0300

    Update the contents of PlantList

--- src/components/PlantList.jsx
+++ src/components/PlantList.jsx
@@ -1,15 +1,21 @@
-import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList({ plants, onToggleStock }) {
+function PlantList({ plants = [] }) {
+  if (plants.length === 0) {
+    return (
+      <ul className="cards">
+        <li className="empty-state">
+          <p>No plants found</p>
+          <span>Try a different search or add a new plant above</span>
+        </li>
+      </ul>
+    );
+  }
+
   return (
     <ul className="cards">
       {plants.map((plant) => (
-        <PlantCard
-          key={plant.id}
-          plant={plant}
-          onToggleStock={onToggleStock}
-        />
+        <PlantCard key={plant.id} plant={plant} />
       ))}
     </ul>
   );
